b510fd1d575236aba6e1bc6cdd2e2c14cad1c16a
jacktoneowidi <kingsowidi@gmail.com>
2024-03-27 21:53:42 +0300

    learning javascript boolean value

---
+++ Java-Script/lesson_1/index.js
@@ -0,0 +1,26 @@
+// practicing variables
+
+
+let Nairobi = 'green-army';
+console.log(Nairobi);
+
+// boolean (Logical NOT !) 
+
+
+
+console.log(!true); //this will print out false
+console.log(!false); // this will print out true
+
+
+// (Logical AND $$)
+
+console.log(false && true );  //prints out false
+console.log(false && false);  //prints out false
+console.log (true && false);  //prints out false
+console.log(true && true);    //prints out true
+
+
+// (Logical OR || ) 
+
+console.log(false || true );  //prints out true
+console.log(true || false );  //prints out true

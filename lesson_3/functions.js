89848846ca3a5e7bb58d16fe0e5ff0534a2aba78
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2024-04-01 21:42:43 +0300

    Update functions.js

--- lesson_3/functions.js
+++ lesson_3/functions.js
@@ -3,3 +3,13 @@ function sayHi(){
 }
 
 sayHi();
+function metro(username, music, ski){
+    console.log(`metroboomin is such a great ${username}`);
+    console.log(`future came through in this ${music}`);
+    console.log(`trav came along way in the album ${ski}` )
+
+}
+metro('producer', 'album', 'fien' )
+
+
+
